b5b2585f9b3405750750b0b3e8c258456e4455a7
grace-hong <gracehong9@gmail.com>
2019-03-29 18:54:35 -0400

    Added postgresql database code

--- server.py
+++ server.py
@@ -1,12 +1,14 @@
 from flask import Flask, render_template
 import os
-import mysql.connector
+#import mysql.connector
 
 
 app = Flask(__name__)
 
-db = mysql.connector.connect(host = "localhost", user = "root", passwd = "pass")
-cursor = db.cursor()
+conn = psycopg2.connect("dbname=inventory user=gracehong password=WoChiPingGuo4868")
+cursor = conn.cursor()
+#db = mysql.connector.connect(host = "localhost", user = "root", passwd = "pass")
+#cursor = db.cursor()
 
 @app.route("/")
 def main():
@@ -14,8 +16,8 @@ def main():
 
 @app.route("/<item>")
 def search(item):
-    cursor.execute("SELECT * FROM food WHERE foodname='%s'" % item)
-    query = cursor.fetchone()
+    #cursor.execute("SELECT * FROM food WHERE foodname='%s'" % item)
+    #query = cursor.fetchone()
     if query == None:
         html = "No such item was found"
     else:

aaeb34afdb55e18b9b440ea08b42fefee49acb96
kellypetrino <42554186+kellypetrino@users.noreply.github.com>
2019-03-28 21:44:17 -0400

    Add files via upload

--- server.py
+++ server.py
@@ -1,10 +1,16 @@
 from flask import Flask, render_template
 import os
+import mysql.connector
 
 app = Flask(__name__)
 
 @app.route("/")
 def main():
+	db = mysql.connect {
+		host = "localhost",
+		user = "root",
+		passwd = "pass"
+	}
     return render_template('index.html')
 
 if __name__ == "__main__":

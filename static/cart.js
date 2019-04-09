a3e72884038f61c941e5302bed220a4ddf68539e
nihe07 <46404033+nihe07@users.noreply.github.com>
2019-04-09 14:16:46 -0400

    Update cart.js

--- static/cart.js
+++ static/cart.js
@@ -37,7 +37,7 @@ function removeCartItem(event) {
 
 function addToCartClicked(event) {
     var button = event.target
-    var shopItem = button.parentElement.parentElement
+    var shopItem = button.parentElement
     var title = shopItem.getElementsByClassName('shop-item-title')[0].innerText
     var price = shopItem.getElementsByClassName('shop-item-price')[0].innerText
     var imageSrc = shopItem.getElementsByClassName('shop-item-image')[0].src

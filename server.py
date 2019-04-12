4743314873881c91d1794012bc6db8f932453c14
grace-hong <gracehong9@gmail.com>
2019-04-12 00:43:31 -0400

    Update server.py

--- server.py
+++ server.py
@@ -101,8 +101,6 @@ def getItem(item):
     return "No results found."
   for re in results:
       if item.lower() in str(re[0]).lower():
-        retVal = retVal + 'lunch check: ' + request.form.get('lunchcheck');
-        retVal = retVal + 'dinner check: ' + request.form.get('dinnercheck');
         if request.POST.get('lunchcheck', True): #print lunch item
           if re[3].lower() == "lunch":
             retVal = retVal + (pre + str(re[2]) + post_image + str(re[0]) + post_title + "{0:.2f}".format(re[1]) + post)

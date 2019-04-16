2b6d0939033e9159a5dc7eb6d62ae968d799e98e
nihe07 <46404033+nihe07@users.noreply.github.com>
2019-04-16 11:20:54 -0400

    Update cart.js

--- static/cart.js
+++ static/cart.js
@@ -53,11 +53,12 @@ function addItemToCart(title, price) {
     var cartItemNames = cartItems.getElementsByClassName('cart-item-title')
   
     var cartBlockContents = `
-    <div>
+    <div class = "cart-item">
 
-    <span>${title}</span>
-    <span>${price}</span>
+    <span class="cart-item-title">${title}</span>
+    <span class="cart-price">${price}</span>
     
+
      <button class="btn btn-danger fas fa-minus" type="button"></button>
 
     </div>

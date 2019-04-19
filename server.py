67664b8e1fddce187867bea7dcaab4641c64f720
devrajarjun <45363658+devrajarjun@users.noreply.github.com>
2019-04-19 19:10:20 -0400

    Add files via upload

--- server.py
+++ server.py
@@ -149,7 +149,7 @@ def getItemsFromCategory(catg):
   if len(results) == 0:
     return "No results found."
   for re in results:
-    retVal = retVal + (pre + str(re[2]) + post_image + str(re[0]) + post_title + "{0:.2f}".format(re[1]) + post1 + str(re[0]) + post2)
+    retVal = retVal + (pre + str(re[2]) + post_image + str(re[0]) + post_title + "{0:.2f}".format(re[1]) + post1 + str(re[0]) + ">" + post2)
 
   return render_template("category.html", resultList = Markup(retVal))
 

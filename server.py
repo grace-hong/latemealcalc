27629d9a8de438703b766026cc8463f5c0cb62c0
grace-hong <gracehong9@gmail.com>
2019-04-19 20:34:21 -0400

    Update server.py

--- server.py
+++ server.py
@@ -141,7 +141,7 @@ def getItemsFromCategory(catg):
       <td class="shop-item-title"><h5>'''
   post_title = '''</h5></td>
       <td class="shop-item-price"><h5>$'''
-  post1 = '''</h5></td><td class="button" onclick="javascript:window.location='/addItem/'">'''
+  post1 = '''</h5></td><td class="button" onclick="javascript:window.location='/addItem/{item}'">'''
   post2 = '''
         <button class="btn btn-primary shop-item-button fas fa-plus"></button>
       </td>
@@ -151,7 +151,7 @@ def getItemsFromCategory(catg):
   for re in results:
     retVal = retVal + (pre + str(re[2]) + post_image + str(re[0]) + post_title + "{0:.2f}".format(re[1]) + post1 + str(re[0]) + '''''' + post2)
 
-  return render_template("category.html", resultList = Markup(retVal))
+  return render_template("category.html", resultList = Markup(retVal), item = Markup(str(re[0])))
 
 @app.route("/checkout")
 def checkout():
@@ -169,6 +169,10 @@ def getsession():
 
 @app.route("/addItem/<item>")
 def addItem(item):
+  if 'cart' not in request.session:
+    request.session['cart'] = {str(item)}
+  else:
+    request.session['cart'].update(str(item))
   return str(item)
 
 if __name__ == "__main__":

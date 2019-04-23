9f6790b2e69c26af17b02b0d140ac695938897f1
grace-hong <gracehong9@gmail.com>
2019-04-23 16:17:22 -0400

    Update cart.js

--- static/cart.js
+++ static/cart.js
@@ -94,6 +94,7 @@ function updateCartTotal() {
     }*/
     
     var priceElement = document.getElementsByClassName('total-price')[0].textContent
+    alert("maxPrice = "  + maxPrice + " total = " + priceElement);
     parseFloat(priceElement.replace('$', ''))
     total = Math.round(priceElement * 100) / 100
     
@@ -103,7 +104,7 @@ function updateCartTotal() {
     else {
         maxPrice = 7
     }
-    alert("maxPrice = "  + maxPrice + " total = " + total);
+    
     // COLOR FOR TOTAL WHEN IT BECOMES GREATER THAN LATEMEAL PRICE
     if (total > maxPrice) {
     	document.getElementsByClassName('cart-total-price')[0].style.color = "red";

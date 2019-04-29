afb7d903a552acaef407a9eda6df0cbdede0a839
kellypetrino <42554186+kellypetrino@users.noreply.github.com>
2019-04-28 23:54:53 -0400

    Update server.py

--- server.py
+++ server.py
@@ -176,14 +176,15 @@ def getFavorites():
   
   diff = budget - sum 
   
-  cursor.execute("SELECT name, price, time FROM food WHERE time!=(%s) AND price <= (%s) ORDER BY count DESC LIMIT 3", (selector, diff, ))
+  cursor.execute("SELECT name, price, time, category FROM food WHERE time!=(%s) AND price <= (%s) AND category != (%s) ORDER BY count DESC LIMIT 10", (selector, diff, "unicorn", ))
   results2 = cursor.fetchall()
   for re in results2:
-    pre4 = '''<div class = "cart-block"><div class = "cart-item"> <span class="cart-item-title">'''
-    post_title4 = '''</span> <span class="cart-price">$'''
-    post_price4 = '''</span> <button class="btn btn-danger fa fa-plus" type="button" onclick="javascript:window.location='/addItem/favorites/'''
-    post_window4 = ''''"></button></div></div>'''
-    retVal4 = retVal4 + (pre4 + str(re[0]) + post_title4 + "{:.2f}".format(re[1]) + post_price4 + str(re[0]) + post_window4)
+    if (str(re[0]) not in str(retVal2)):
+      pre4 = '''<div class = "cart-block"><div class = "cart-item"> <span class="cart-item-title">'''
+      post_title4 = '''</span> <span class="cart-price">$'''
+      post_price4 = '''</span> <button class="btn btn-danger fa fa-plus" type="button" onclick="javascript:window.location='/addItem/favorites/'''
+      post_window4 = ''''"></button></div></div>'''
+      retVal4 = retVal4 + (pre4 + str(re[0]) + post_title4 + "{:.2f}".format(re[1]) + post_price4 + str(re[0]) + post_window4)
     
 
   if cart.get(session['uid']) == None:
@@ -227,14 +228,15 @@ def getInfo():
   retVal3 = '''$''' + "{:.2f}".format(sum)
   
   retVal4 = ""  
-  cursor.execute("SELECT name, price, time FROM food WHERE time!=(%s) AND price <= (%s) ORDER BY count DESC LIMIT 3", (selector, diff, ))
+  cursor.execute("SELECT name, price, time, category FROM food WHERE time!=(%s) AND price <= (%s) AND category != (%s) ORDER BY count DESC LIMIT 10", (selector, diff, "unicorn", ))
   results2 = cursor.fetchall()
   for re in results2:
-    pre4 = '''<div class = "cart-block"><div class = "cart-item"> <span class="cart-item-title">'''
-    post_title4 = '''</span> <span class="cart-price">$'''
-    post_price4 = '''</span> <button class="btn btn-danger fa fa-plus" type="button" onclick="javascript:window.location='/addItem/info/'''
-    post_window4 = ''''"></button></div></div>'''
-    retVal4 = retVal4 + (pre4 + str(re[0]) + post_title4 + "{:.2f}".format(re[1]) + post_price4 + str(re[0]) + post_window4)
+    if (str(re[0]) not in str(retVal2)):
+      pre4 = '''<div class = "cart-block"><div class = "cart-item"> <span class="cart-item-title">'''
+      post_title4 = '''</span> <span class="cart-price">$'''
+      post_price4 = '''</span> <button class="btn btn-danger fa fa-plus" type="button" onclick="javascript:window.location='/addItem/info/'''
+      post_window4 = ''''"></button></div></div>'''
+      retVal4 = retVal4 + (pre4 + str(re[0]) + post_title4 + "{:.2f}".format(re[1]) + post_price4 + str(re[0]) + post_window4)
     
 
   if cart.get(session['uid']) == None:

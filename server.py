361d12b3914f2c77c06bb938056f7fb46e71fb74
devrajarjun <45363658+devrajarjun@users.noreply.github.com>
2019-04-29 15:26:27 -0400

    Update server.py

--- server.py
+++ server.py
@@ -44,6 +44,7 @@ conn.commit()
 cart = {}
 time = {}
 packaged = {}
+combos = {}
 
 comboMain = ["Neapolitan Cheese Pizza", "Burrito", "Burrito Bowl", "Carved Entree with 2 sides", "Chicken and Waffles"]
 comboAdd = ["Soda 16oz", "Milk Half Pint", "Apple", "Banana", "Grapefruit", "Nectarine", "Orange", "Peach", "Pear", "Plum", "Cookie (unwrapped)", "Tuna Salad", "Egg Salad"]

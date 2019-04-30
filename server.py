ec58be01dfcba6648ada41c0a03469742967cd27
ewang2581 <48871805+ewang2581@users.noreply.github.com>
2019-04-30 03:06:41 -0400

    Update server.py

--- server.py
+++ server.py
@@ -348,12 +348,12 @@ def getItem(item):
     retVal6 = '''      <script type = "text/javascript">
          confirm("Want to continue?")
       </script>'''
-    return render_template("results.html", resultList2 = Markup(retVal2), resultList3 = Markup(retVal3), resultList4 = Markup(retVal4), surplus = "${:.2f}".format(diff), packagedconfirm = Markup(retVal6),)
+    return render_template("results.html", resultList = Markup(retVal), resultList2 = Markup(retVal2), resultList3 = Markup(retVal3), surplus = "${:.2f}".format(diff), packagedconfirm = Markup(retVal6),)
   if packaged.get(session['uid']) >= 2 and diff < 0:
     retVal6 = '''      <script type = "text/javascript">
          confirm("Want to continue?")
       </script>'''
-    return render_template("results.html", resultList2 = Markup(retVal2), resultList3 = Markup(retVal3), resultList4 = Markup(retVal4), diffOver = "${:.2f}".format(diff*-1), packagedconfirm = Markup(retVal6),)
+    return render_template("results.html", resultList = Markup(retVal), resultList2 = Markup(retVal2), resultList3 = Markup(retVal3), diffOver = "${:.2f}".format(diff*-1), packagedconfirm = Markup(retVal6),)
   if diff >= 0:
     return render_template("results.html", resultList = Markup(retVal), resultList2 = Markup(retVal2), resultList3 = Markup(retVal3), resultList4 = Markup(retVal4), surplus = "${:.2f}".format(diff),)
   else:
@@ -450,12 +450,12 @@ def getItemsFromCategory(catg):
     retVal6 = '''      <script type = "text/javascript">
          confirm("Want to continue?")
       </script>'''
-    return render_template("category.html", resultList2 = Markup(retVal2), resultList3 = Markup(retVal3), resultList4 = Markup(retVal4), surplus = "${:.2f}".format(diff), packagedconfirm = Markup(retVal6),)
+    return render_template("category.html", resultList = Markup(retVal), resultList2 = Markup(retVal2), resultList3 = Markup(retVal3), surplus = "${:.2f}".format(diff), packagedconfirm = Markup(retVal6),)
   if packaged.get(session['uid']) >= 2 and diff < 0:
     retVal6 = '''      <script type = "text/javascript">
          confirm("Want to continue?")
       </script>'''
-    return render_template("category.html", resultList2 = Markup(retVal2), resultList3 = Markup(retVal3), resultList4 = Markup(retVal4), diffOver = "${:.2f}".format(diff*-1), packagedconfirm = Markup(retVal6),)
+    return render_template("category.html", resultList = Markup(retVal), resultList2 = Markup(retVal2), resultList3 = Markup(retVal3), diffOver = "${:.2f}".format(diff*-1), packagedconfirm = Markup(retVal6),)
   if diff >= 0:
     print("This 2")
     print(comboRet)

60f36a671b6e3169cb256f1c22de93cad0ae9f9a
devrajarjun <45363658+devrajarjun@users.noreply.github.com>
2019-04-30 00:59:02 -0400

    Update server.py

--- server.py
+++ server.py
@@ -456,7 +456,7 @@ def getItemsFromCategory(catg):
          confirm("Want to continue?")
       </script>'''
     return render_template("info.html", resultList2 = Markup(retVal2), resultList3 = Markup(retVal3), resultList4 = Markup(retVal4), diffOver = "${:.2f}".format(diff*-1), packagedconfirm = Markup(retVal6),)
-  if diff >= 0:
+  if diff > 0:
     print("This 2")
     return render_template("category.html", resultList = Markup(retVal), resultList2 = Markup(retVal2), resultList3 = Markup(retVal3), resultList4 = Markup(retVal4), surplus = "${:.2f}".format(diff), resultsList5 = Markup(comboRet))
   else:

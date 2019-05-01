574fefcf3cdd036f58e9313687b13becd835aa28
grace-hong <gracehong9@gmail.com>
2019-05-01 13:52:42 -0400

    Update server.py

--- server.py
+++ server.py
@@ -404,9 +404,9 @@ def getSpecials():
     if (str(re[0]) not in str(retVal2)):
       pre4 = '''<div class = "cart-block"><div class = "cart-item"> <span class="cart-item-title">'''
       post_title4 = '''</span> <span class="cart-price">$'''
-      post_price4 = '''</span> <button class="btn btn-primary fa fa-plus" type="button" style="font-size: 10px; border-radius:3rem;" onclick="javascript:window.location='/addItem/item/'''
+      post_price4 = '''</span> <button class="btn btn-primary fa fa-plus" type="button" style="font-size: 10px; border-radius:3rem;" onclick="javascript:window.location='/addItem/item/Late '''
       post_window4 = ''''"></button></div></div><br>'''
-      retVal4 = retVal4 + (pre4 + str(re[0]) + post_title4 + "{:.2f}".format(re[1]) + post_price4 + str(item) + "/" + str(re[0]) + post_window4)
+      retVal4 = retVal4 + (pre4 + str(re[0]) + post_title4 + "{:.2f}".format(re[1]) + post_price4 + str(selector) + " Special/" + str(re[0]) + post_window4)
 
   if cart.get(session['uid']) == None:
     return render_template("results.html", resultList = Markup(retVal))

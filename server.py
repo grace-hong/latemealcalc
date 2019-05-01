ca2ed8b2b8df565c3c06ee53f1422bddd07ed08b
devrajarjun <45363658+devrajarjun@users.noreply.github.com>
2019-05-01 02:23:29 -0400

    Update server.py

--- server.py
+++ server.py
@@ -430,8 +430,7 @@ def getItemsFromCategory(catg):
 
   # comboRet = ''''''
   if combos.get(session['uid']) == 1 and combosFull.get(session['uid']) != 1:
-    comboRet = '''<script>alert('You have entered a combo item! Please navigate to our combos page.')</script>'''
-    '''
+    comboRet = '''
     <div id='modal_dialog' style='background-color: #000000;'>
     	<div class='title' style='font-weight: 500; font-style: italic; color: white'>
     	</div>

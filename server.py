df8d5e9942084451c83d059d9941dd6d07a1e6b6
ewang2581 <48871805+ewang2581@users.noreply.github.com>
2019-05-05 02:09:43 -0400

    Update server.py

--- server.py
+++ server.py
@@ -135,13 +135,23 @@ def main():
 			}
 		</script> '''
     needAlert[session['uid']] = 0
-    return render_template("index.html", resultList2 = Markup(retVal2), resultList3 = Markup(retVal3), resultList4 = Markup(retVal4), surplus = "${:.2f}".format(diff), packagedconfirm = Markup(retVal6),)
+    if time[session['uid']] == 0:
+	retVal7 = '''<script> document.getElementById("timeBalance").innerHTML = "$7.00" </script>'''
+	#return render_template("index.html", resultList2 = Markup(retVal2), resultList3 = Markup(retVal3), resultList4 = Markup(retVal4), surplus = "${:.2f}".format(diff), packagedconfirm = Markup(retVal6), timesetting = Markup(retVal7),)
+    else:
+	retVal7 = '''<script> document.getElementById("timeBalance").innerHTML = "$6.00" </script> '''
+    return render_template("index.html", resultList2 = Markup(retVal2), resultList3 = Markup(retVal3), resultList4 = Markup(retVal4), surplus = "${:.2f}".format(diff), packagedconfirm = Markup(retVal6), timesetting = Markup(retVal7),)
   if packaged.get(session['uid']) == 2 and needAlert.get(session['uid']) == 1 and diff < 0:
     #retVal6 = ''' <script>confirm("You have reached the 2 packaged goods limit. Want to continue?") </script>'''
     retVal6 = ''' <script> if (alert("2 packaged goods only! Please try another item.")) {
 			} </script> '''
     needAlert[session['uid']] = 0
-    return render_template("index.html", resultList2 = Markup(retVal2), resultList3 = Markup(retVal3), resultList4 = Markup(retVal4), diffOver = "${:.2f}".format(diff*-1), packagedconfirm = Markup(retVal6),)
+    if time[session['uid']] == 0:
+	retVal7 = '''<script> document.getElementById("timeBalance").innerHTML = "$7.00" </script>'''
+    	#return render_template("index.html", resultList2 = Markup(retVal2), resultList3 = Markup(retVal3), resultList4 = Markup(retVal4), diffOver = "${:.2f}".format(diff*-1), packagedconfirm = Markup(retVal6), timesetting = Markup(retVal7),)
+    else: 
+	retVal7 = '''<script> document.getElementById("timeBalance").innerHTML = "$6.00" </script> '''
+    return render_template("index.html", resultList2 = Markup(retVal2), resultList3 = Markup(retVal3), resultList4 = Markup(retVal4), diffOver = "${:.2f}".format(diff*-1), packagedconfirm = Markup(retVal6), timesetting = Markup(retVal7),)
   if diff >= 0:
     return render_template("index.html", resultList2 = Markup(retVal2), resultList3 = Markup(retVal3), resultList4 = Markup(retVal4), surplus = "${:.2f}".format(diff),)
   else:
